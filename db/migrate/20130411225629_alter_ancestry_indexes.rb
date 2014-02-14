de89483ea30d6227b52fab55a442710766f4c176
Chris Keathley <spyc3r@gmail.com>
2014-02-13 22:31:41 -0500

    Fix more broken migrations

--- db/migrate/20130411225629_alter_ancestry_indexes.rb
+++ db/migrate/20130411225629_alter_ancestry_indexes.rb
@@ -1,7 +1,7 @@
 class AlterAncestryIndexes < ActiveRecord::Migration
   def up
-    remove_index :taxa, :ancestry
-    remove_index :places, :ancestry
+    # remove_index :taxa, :ancestry
+    # remove_index :places, :ancestry
     execute "CREATE INDEX index_taxa_on_ancestry ON taxa(ancestry text_pattern_ops)"
     execute "CREATE INDEX index_places_on_ancestry ON places(ancestry text_pattern_ops)"
   end
@@ -9,7 +9,7 @@ class AlterAncestryIndexes < ActiveRecord::Migration
   def down
     execute "DROP INDEX index_taxa_on_ancestry"
     execute "DROP INDEX index_places_on_ancestry"
-    add_index :taxa, :ancestry
-    add_index :places, :ancestry
+    # add_index :taxa, :ancestry
+    # add_index :places, :ancestry
   end
 end
